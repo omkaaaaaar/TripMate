946d8e7c23fbdeb3ee3860364259800979763ab4
omkaaaaaar <omkarpatkarr@gmail.com>
2025-08-06 21:50:06 +0530

    Update settings.py

--- tripmate_project/settings.py
+++ tripmate_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "tripmate_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        'DIRS': [BASE_DIR / 'planner' / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
