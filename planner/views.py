0990d964ed0f040788a98da445d1b9e0074ac7a4
omkaaaaaar <omkarpatkarr@gmail.com>
2025-08-06 21:51:29 +0530

    Update views.py

--- planner/views.py
+++ planner/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render, redirect
+
+def home(request):
+    if request.method == 'POST':
+        destination = request.POST.get('destination')
+        days = request.POST.get('days')
+
+        # Redirect to /plan/ with data in query string
+        return redirect(f'/plan/?destination={destination}&days={days}')
+
+    return render(request, 'planner/home.html')
