68ca0b3f33b67c2be6a3752fd7b82e3aec2f147b
omkaaaaaar <omkarpatkarr@gmail.com>
2025-08-06 21:51:55 +0530

    Update urls.py

--- tripmate_project/urls.py
+++ tripmate_project/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from planner import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),         # homepage
+    path('plan/', views.plan, name='plan'),    # trip plan (coming next)
 ]
