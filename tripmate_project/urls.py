ac45740dc360d70e36ac85f764468ae26de1d815
omkaaaaaar <omkarpatkarr@gmail.com>
2025-08-07 19:26:53 +0530

    q

--- tripmate_project/urls.py
+++ tripmate_project/urls.py
@@ -22,5 +22,6 @@ from planner import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),         # homepage
-    path('plan/', views.plan, name='plan'),    # trip plan (coming next)
+    path('plan/', views.trip_plan, name='plan'),    # trip plan (coming next)
 ]
+
